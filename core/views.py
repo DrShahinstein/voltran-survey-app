2cee710a1347740f356ce52110387976e8c7c5d6
DrShahinstein <1taylanozturk@protonmail.com>
2024-01-14 18:10:39 +0300

    Create a separate QuestionView in accordance with the dynamic Question rendering idea (see urls.py)

--- core/views.py
+++ core/views.py
@@ -1,13 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
 from django.views import View
-from .models import Question
+from core.models import Question
 
 
 class IndexView(View):
     def get(self, request):
-        questions = Question.objects.all()
+        if Question.objects.exists():
+            return redirect("question-view", pk=1)
+        else:
+            return render(request, "404.html")
+
+
+class QuestionView(View):
+    def get(self, request, pk):
+        try:
+            self.object = self.get_object(pk)
+        except:
+            return redirect("question-view", pk=1)
+
+        context = self.get_context_data()
+        return render(request, "questions.html", context)
+
+    def post(self, request, pk):
+        try:
+            self.object = self.get_object(pk)
+        except:
+            return redirect("question-view", pk=1)
+
+        context = self.get_context_data()
+
+        response = {
+            "success": {"success": "Answer submitted successfully"},
+            "error": {"error": "Invalid request data"},
+        }
+
+        question_text = request.POST.get("question_text")
+        selected_answer = request.POST.get("selected_answer")
+
+        if not question_text or not selected_answer:
+            return JsonResponse(response["error"], status=400)
+
+        question = get_object_or_404(Question, question=question_text)
+
+        self.update_question_answers(question, selected_answer)
+        question.save()
+
+        return render(request, "questions.html", context)
+
+    def get_context_data(self, **kwargs) -> dict[str, any]:
+        question = self.object
         context = {
-            "poll_questions": questions,
+            "question": question,
             "scale_order": [
                 (2, "agree"),
                 (1, "agree"),
@@ -15,5 +59,30 @@ class IndexView(View):
                 (1, "disagree"),
                 (2, "disagree"),
             ],
+            **kwargs,
         }
-        return render(request, "index.html", context)
+        return context
+
+    def get_object(self, pk):
+        return get_object_or_404(Question, pk=pk)
+
+    def update_question_answers(self, question, selected_answer):
+        if selected_answer == "strongly agree":
+            question.overall_strongly_agree_answers += 1
+        elif selected_answer == "agree":
+            question.overall_agree_answers += 1
+        elif selected_answer == "neutral":
+            question.overall_neutral_answers += 1
+        elif selected_answer == "disagree":
+            question.overall_disagree_answers += 1
+        elif selected_answer == "strongly disagree":
+            question.overall_strongly_disagree_answers += 1
+
+
+class ResultsView(View):
+    def get(self, request):
+        questions = Question.objects.all()
+        context = {
+            "questions": questions,
+        }
+        return render(request, "results.html", context)
