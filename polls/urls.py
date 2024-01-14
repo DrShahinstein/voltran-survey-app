4a849b18fe5b46108b540ceba109dcd15894be0c
DrShahinstein <1taylanozturk@protonmail.com>
2024-01-14 18:11:41 +0300

    Render the questions with dynamic routing

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import IndexView
-
+from core.views import IndexView, QuestionView, ResultsView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", IndexView.as_view(), name="index page"),
+    path("", IndexView.as_view(), name="index"),
+    path("question/<int:pk>/", QuestionView.as_view(), name="question-view"),
+    path("results/", ResultsView.as_view(), name="results"),
 ]
