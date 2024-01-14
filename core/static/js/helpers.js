85bfb5abd8f49d2af0b84b4d7a2fe803f0e53393
DrShahinstein <1taylanozturk@protonmail.com>
2024-01-14 18:03:22 +0300

    Add new helper constants in helpers.js

--- core/static/js/helpers.js
+++ core/static/js/helpers.js
@@ -1,9 +1,21 @@
-const radioTypes = ["radio-agree", "radio-disagree", "radio-neutral"];
+/* helpers.js */
 
-const selectedClassMap = {
+const RADIO_TYPES = ["radio-agree", "radio-disagree", "radio-neutral"];
+
+const RADIO_SELECTED_MAPPING = {
   "radio-agree": "radio-agree-selected",
   "radio-disagree": "radio-disagree-selected",
   "radio-neutral": "radio-neutral-selected",
 };
 
-const QUERY_RADIOS = ".poll-container:not(.d-none) .radio-span";
+const QUERY_RADIOS = ".radio-span";
+
+const QUERY_INPUT_ANSWER = "input-selected-answer";
+
+const ANSWER_MAPPING = {
+  "radio-0": "strongly agree",
+  "radio-1": "agree",
+  "radio-2": "neutral",
+  "radio-3": "disagree",
+  "radio-4": "strongly disagree",
+};
