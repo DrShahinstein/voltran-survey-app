e0866fb82d7de3a8b11b85edae418ec0fdeb5865
DrShahinstein <1taylanozturk@protonmail.com>
2024-01-14 20:42:21 +0300

    Bug-fix main.js

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,12 +1,15 @@
 /* main.js */
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Management of radio buttons
   const radios = document.querySelectorAll(QUERY_RADIOS);
   manageRadios(radios);
 });
 
 function manageRadios(radios) {
+  if (!radios) {
+    return;
+  }
+
   radios.forEach((radio) => {
     radio.addEventListener("click", () => {
       radios.forEach((otherRadio) => {
@@ -15,15 +18,15 @@ function manageRadios(radios) {
 
       radio.classList.add("radio-disabled");
 
-      const answer = expressAnswer(radio.classList);
-      document.getElementById(QUERY_INPUT_ANSWER).value = answer;
-
       for (const rType of RADIO_TYPES) {
         if (radio.classList.contains(rType)) {
           radio.classList.add(RADIO_SELECTED_MAPPING[rType]);
           break;
         }
       }
+
+      const answer = expressAnswer(radio.classList);
+      document.getElementById(QUERY_INPUT_ANSWER).value = answer;
     });
   });
 }
@@ -34,5 +37,5 @@ function expressAnswer(radioClassList) {
       return ANSWER_MAPPING[className];
     }
   }
-  return "";
+  return "neutral";
 }
